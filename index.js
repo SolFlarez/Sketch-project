776991f75019361b1c269392434256e51adb0393
Chance <chance.felkel@gmail.com>
2023-09-19 13:24:31 -0400

    Grid now dynamically changes based on slider value

--- index.js
+++ index.js
@@ -5,12 +5,7 @@ var output = document.getElementById("demo");
 output.textContent = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
-slider.oninput = function () {
-  output.textContent = this.value;
-  changeGrid(this.value);
-};
-
-let userInputResult = output.textContent;
+// update grid demensions based on slider value
 
 const createGrid = () => {
   // i need to make a 16 x 16 grid of squares divs. Use a loop to create 16 divs inside the grid container
@@ -18,12 +13,25 @@ const createGrid = () => {
   //   userInput = Number(
   //     prompt("How many boxes wide/tall would you like the grid to be?")
   //   );
-  for (i = 0; i < userInputResult * userInputResult; i++) {
+
+  for (i = 0; i < slider.value * slider.value; i++) {
     const gridBox = document.createElement("div");
-    gridBox.style.width = `${650 / userInputResult}px`;
-    gridBox.style.height = `${650 / userInputResult}px`;
+    gridBox.style.width = `${650 / slider.value}px`;
+    gridBox.style.height = `${650 / slider.value}px`;
     gridContainer.appendChild(gridBox);
   }
 };
 
-const changeGridSize = ()
+const defaultGrid = createGrid();
+
+slider.oninput = function () {
+  output.textContent = this.value;
+};
+
+slider.onchange = function () {
+  clearGrid();
+  const currentGrid = createGrid(this.value);
+  return currentGrid;
+};
+
+const clearGrid = () => (gridContainer.innerHTML = "");
